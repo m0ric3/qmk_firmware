ad6a19c380cb6b3485f91273bf17c97072be27e5
Mauricio Guerrero <smauguerrero@gmail.com>
2023-01-05 15:38:24 -0800

    Enable macOS friendly Grave Escape

--- keyboards/cannonkeys/brutalv2_65/keymaps/m0ric3/keymap.c
+++ keyboards/cannonkeys/brutalv2_65/keymaps/m0ric3/keymap.c
@@ -30,7 +30,7 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_BASE] = LAYOUT_all(
-		QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_DEL,
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_DEL,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_PGUP,
 		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
 		SC_LSPO, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          SC_RSPC, KC_UP,   MO(_FN),
@@ -45,4 +45,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		CW_TOGG, _______, _______,                            _______,                            _______, _______,          KC_HOME, KC_PGDN, KC_END
     )
 
+};
+
+
+// Shift + esc = ~
+const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRV));
+
+// GUI + esc = `
+const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_GUI, KC_ESC, KC_GRV);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &tilde_esc_override,
+    &grave_esc_override,
+    NULL
 };
